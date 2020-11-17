917801f7b6618a73e907c1fe6def9ecb8a968867
Spencer Sterling <spencer.sterling2001@gmail.com>
2020-11-16 21:56:28 -0800

    IsWithinRange Tests passing

--- BusinessLogicTests/ValidatorTests.cs
+++ BusinessLogicTests/ValidatorTests.cs
@@ -42,8 +42,8 @@ namespace BusinessLogicTests
         [DataRow(2000, 0, 50000)]
         public void IsWithinRange_NumInInclusiveRange_ReturnTrue(int test, int min, int max)
         {
-            //TODO: Test the IsWithinRange method in the Validator class
-            Assert.Fail();
+            bool result = Validator.IsWithinRange(test, min, max);
+            Assert.IsTrue(result);
         }
 
         [TestMethod]
@@ -52,8 +52,8 @@ namespace BusinessLogicTests
         [DataRow(10000, 0, 5000)]
         public void IsWithinRange_NumOutsideInclusiveRange_ReturnsFalse(int test, int min, int max)
         {
-            //TODO: Test the IsWithinRange method in the Validator class
-            Assert.Fail();
+            bool result = Validator.IsWithinRange(test, min, max);
+            Assert.IsFalse(result);
         }
     }
 }
